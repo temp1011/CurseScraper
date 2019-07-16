97d297d123d44d82844bf36bc53b26411d033ac2
temp1011 <temp1011github@gmail.com>
2019-07-16 23:53:50 +0100

    add accept encoding header
    Doesn't seem to really make much a performance difference. The main cause of waiting is still a handful of very slow responses from server (possibly due to rate limiting). So I need to shorten timeout on requests and retry and/or implement a queue system so things don't block as much.

--- src/download.py
+++ src/download.py
@@ -8,7 +8,8 @@ from typing import Optional, Iterable, List
 import aiohttp
 
 HEADERS = {
-	"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
+	"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36",
+	"Accept-Encoding": "gzip,deflate"
 }
 
 # could use tuple/dict here but this is safer (and not stringly typed!)
